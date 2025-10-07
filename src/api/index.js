f4cea84290674dc92941c2ca82ca2dbb15157f39
Abhay Mishra <159817827+abhay280896@users.noreply.github.com>
2025-10-07 14:01:00 +0000

    change routes in vercel.json

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 // api/index.js
-const serverless = require('serverless-http');
-const app = require('../app');
-const { connectDB } = require('../config/database');
+const serverless = require("serverless-http");
+const app = require("../src/app");
+const { connectDB } = require("../src/config/database");
 
 let isConnected = false;
 
@@ -9,7 +9,7 @@ module.exports = async (req, res) => {
   if (!isConnected) {
     await connectDB();
     isConnected = true;
-    console.log('✅ Database connected');
+    console.log("✅ Database connected");
   }
   const handler = serverless(app);
   return handler(req, res);
